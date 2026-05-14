9d2ec5d0c3b842ff15740463a740838d888c94bb
Nixeld <ethanongchuanming@gmail.com>
2026-05-15 03:32:19 +0800

    v1.0.0

---
+++ DiscoverAllRooms.cs
@@ -0,0 +1,67 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace DiscoverAllRooms;
+
+[BepInPlugin("Nixeld.DiscoverAllRooms", "DiscoverAllRooms", "1.0")]
+public class DiscoverAllRooms : BaseUnityPlugin
+{
+    internal static DiscoverAllRooms Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+    internal Harmony? Harmony { get; set; }
+
+    internal static ConfigEntry<bool> ConfigDiscoverAllRooms { get; private set; } = null!;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        ConfigDiscoverAllRooms = Config.Bind(
+            "General",
+            "DiscoverAllRooms",
+            true,
+            "When enabled, all rooms are revealed on the map at the start of each level.");
+
+        Patch();
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+
+    internal void Patch()
+    {
+        Harmony ??= new Harmony(Info.Metadata.GUID);
+        Harmony.PatchAll();
+    }
+
+    internal void Unpatch()
+    {
+        Harmony?.UnpatchSelf();
+    }
+}
+
+[HarmonyPatch(typeof(LevelGenerator), nameof(LevelGenerator.GenerateDone))]
+internal static class LevelGeneratorDiscoverAllRoomsPatch
+{
+    private static void Postfix()
+    {
+        if (!DiscoverAllRooms.ConfigDiscoverAllRooms.Value)
+        {
+            return;
+        }
+
+        RoomVolume[] rooms = Object.FindObjectsOfType<RoomVolume>();
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            rooms[i].SetExplored();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# R.E.P.O. - Discover All Rooms
+
+Allows you to set all rooms as discovered and reveal them on the map. Compatible with most other mods.
+
+## Installation
+1. Download & install [BepInEx](https://github.com/BepInEx/BepInEx).
+2. Download the [latest release](https://github.com/Nixeld/R.E.P.O.-Discover-All-Rooms/releases) of this mod.
+3. Extract `Nixeld-DiscoverAllRooms.zip` into your `BepInEx/plugins` directory.
+
+## Configuration
+
+Settings can be configured in-game using the [REPOConfig mod](https://thunderstore.io/c/repo/p/nickklmao/REPOConfig/), or manually by:
+
+1. Launch the game with mod installed at least once.
+2. Edit the `Nixeld.DiscoverAllRooms.cfg` file within your `BepInEx/config` directory.
+
+<details>
+<summary>Example Configuration</summary>
+
+```
+## Settings file was created by plugin DiscoverAllRooms v1.0
+## Plugin GUID: Nixeld.DiscoverAllRooms
+
+[General]
+
+## When enabled, all rooms are revealed on the map at the start of each level.
+# Setting type: Boolean
+# Default value: true
+DiscoverAllRooms = true
+
+```
+
+</details>
